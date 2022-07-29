2929d642491247b5324e8b73dc2c50d255b003c7
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-07-29 23:55:12 +0900

    [Chore] 프로젝트 세팅

---
+++ Weather-Clone/Weather-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Clone
+//
+//  Created by 강윤서 on 2022/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
